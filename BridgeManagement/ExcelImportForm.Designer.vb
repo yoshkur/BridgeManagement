75eff31ca515e1655a54d8277545a8ff38d8888d
yoshkur <yosh@1997.jukuin.keio.ac.jp>
2016-02-18 02:57:50 +0900

    点検データ取り込みフォーム動作。

--- BridgeManagement/ExcelImportForm.Designer.vb
+++ BridgeManagement/ExcelImportForm.Designer.vb
@@ -24,11 +24,6 @@ Partial Class ExcelImportForm
 	Private Sub InitializeComponent()
 		Me.components = New System.ComponentModel.Container()
 		Me.DataGridView1 = New System.Windows.Forms.DataGridView()
-		Me.BridgemanagementExcelImportDataSet = New BridgeManagement.bridgemanagementExcelImportDataSet()
-		Me.InspectionexcelBindingSource = New System.Windows.Forms.BindingSource(Me.components)
-		Me.InspectionexcelTableAdapter = New BridgeManagement.bridgemanagementExcelImportDataSetTableAdapters.inspectionexcelTableAdapter()
-		Me.ComboBox1 = New System.Windows.Forms.ComboBox()
-		Me.ComboBox2 = New System.Windows.Forms.ComboBox()
 		Me.ErrorDataGridViewCheckBoxColumn = New System.Windows.Forms.DataGridViewCheckBoxColumn()
 		Me.MessageDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
 		Me.IdentDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -46,9 +41,13 @@ Partial Class ExcelImportForm
 		Me.UndermaterialDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
 		Me.BearingDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
 		Me.FaceDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
+		Me.InspectionexcelBindingSource = New System.Windows.Forms.BindingSource(Me.components)
+		Me.BridgemanagementExcelImportDataSet = New BridgeManagement.bridgemanagementExcelImportDataSet()
+		Me.InspectionexcelTableAdapter = New BridgeManagement.bridgemanagementExcelImportDataSetTableAdapters.inspectionexcelTableAdapter()
+		Me.ImportButton = New System.Windows.Forms.Button()
 		CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
-		CType(Me.BridgemanagementExcelImportDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
 		CType(Me.InspectionexcelBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
+		CType(Me.BridgemanagementExcelImportDataSet, System.ComponentModel.ISupportInitialize).BeginInit()
 		Me.SuspendLayout()
 		'
 		'DataGridView1
@@ -65,36 +64,6 @@ Partial Class ExcelImportForm
 		Me.DataGridView1.RowTemplate.Height = 21
 		Me.DataGridView1.Size = New System.Drawing.Size(1058, 372)
 		Me.DataGridView1.TabIndex = 0
-		'
-		'BridgemanagementExcelImportDataSet
-		'
-		Me.BridgemanagementExcelImportDataSet.DataSetName = "bridgemanagementExcelImportDataSet"
-		Me.BridgemanagementExcelImportDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
-		'
-		'InspectionexcelBindingSource
-		'
-		Me.InspectionexcelBindingSource.DataMember = "inspectionexcel"
-		Me.InspectionexcelBindingSource.DataSource = Me.BridgemanagementExcelImportDataSet
-		'
-		'InspectionexcelTableAdapter
-		'
-		Me.InspectionexcelTableAdapter.ClearBeforeFill = True
-		'
-		'ComboBox1
-		'
-		Me.ComboBox1.FormattingEnabled = True
-		Me.ComboBox1.Location = New System.Drawing.Point(26, 13)
-		Me.ComboBox1.Name = "ComboBox1"
-		Me.ComboBox1.Size = New System.Drawing.Size(398, 20)
-		Me.ComboBox1.TabIndex = 1
-		'
-		'ComboBox2
-		'
-		Me.ComboBox2.FormattingEnabled = True
-		Me.ComboBox2.Location = New System.Drawing.Point(481, 13)
-		Me.ComboBox2.Name = "ComboBox2"
-		Me.ComboBox2.Size = New System.Drawing.Size(239, 20)
-		Me.ComboBox2.TabIndex = 2
 		'
 		'ErrorDataGridViewCheckBoxColumn
 		'
@@ -216,19 +185,41 @@ Partial Class ExcelImportForm
 		Me.FaceDataGridViewTextBoxColumn.Name = "FaceDataGridViewTextBoxColumn"
 		Me.FaceDataGridViewTextBoxColumn.ReadOnly = True
 		'
+		'InspectionexcelBindingSource
+		'
+		Me.InspectionexcelBindingSource.DataMember = "inspectionexcel"
+		Me.InspectionexcelBindingSource.DataSource = Me.BridgemanagementExcelImportDataSet
+		'
+		'BridgemanagementExcelImportDataSet
+		'
+		Me.BridgemanagementExcelImportDataSet.DataSetName = "bridgemanagementExcelImportDataSet"
+		Me.BridgemanagementExcelImportDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema
+		'
+		'InspectionexcelTableAdapter
+		'
+		Me.InspectionexcelTableAdapter.ClearBeforeFill = True
+		'
+		'ImportButton
+		'
+		Me.ImportButton.Location = New System.Drawing.Point(26, 13)
+		Me.ImportButton.Name = "ImportButton"
+		Me.ImportButton.Size = New System.Drawing.Size(75, 23)
+		Me.ImportButton.TabIndex = 1
+		Me.ImportButton.Text = "データ取り込み"
+		Me.ImportButton.UseVisualStyleBackColor = True
+		'
 		'ExcelImportForm
 		'
 		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
 		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
 		Me.ClientSize = New System.Drawing.Size(1096, 442)
-		Me.Controls.Add(Me.ComboBox2)
-		Me.Controls.Add(Me.ComboBox1)
+		Me.Controls.Add(Me.ImportButton)
 		Me.Controls.Add(Me.DataGridView1)
 		Me.Name = "ExcelImportForm"
 		Me.Text = "ExcelImportForm"
 		CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
-		CType(Me.BridgemanagementExcelImportDataSet, System.ComponentModel.ISupportInitialize).EndInit()
 		CType(Me.InspectionexcelBindingSource, System.ComponentModel.ISupportInitialize).EndInit()
+		CType(Me.BridgemanagementExcelImportDataSet, System.ComponentModel.ISupportInitialize).EndInit()
 		Me.ResumeLayout(False)
 
 	End Sub
@@ -237,8 +228,6 @@ Partial Class ExcelImportForm
 	Friend WithEvents BridgemanagementExcelImportDataSet As bridgemanagementExcelImportDataSet
 	Friend WithEvents InspectionexcelBindingSource As BindingSource
 	Friend WithEvents InspectionexcelTableAdapter As bridgemanagementExcelImportDataSetTableAdapters.inspectionexcelTableAdapter
-	Friend WithEvents ComboBox1 As ComboBox
-	Friend WithEvents ComboBox2 As ComboBox
 	Friend WithEvents ErrorDataGridViewCheckBoxColumn As DataGridViewCheckBoxColumn
 	Friend WithEvents MessageDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
 	Friend WithEvents IdentDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
@@ -256,4 +245,5 @@ Partial Class ExcelImportForm
 	Friend WithEvents UndermaterialDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
 	Friend WithEvents BearingDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
 	Friend WithEvents FaceDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
+	Friend WithEvents ImportButton As Button
 End Class
--- BridgeManagement/ExcelImportForm.vb
+++ BridgeManagement/ExcelImportForm.vb
@@ -11,18 +11,111 @@ Public Class ExcelImportForm
 	Private Sub getInspectionData()
 		Try
 			Dim db As New bridgemanagementEntities
+			db.inspectionexcel.RemoveRange(db.inspectionexcel)
 			Dim setting As Setting = Setting.getInstance
 			Dim directorys = Directory.GetDirectories(setting.WorkDirectoryPath)
-			db.inspectionexcel.RemoveRange(db.inspectionexcel)
+			Dim _error As Boolean = False
 			For Each datadir In directorys
 				Dim i As Integer = 0
 				Dim inspectionExcel As inspectionexcel = New InspectionExcelBuilder(i, datadir).getInstance
 				db.inspectionexcel.Add(inspectionExcel)
+				_error = _error OrElse inspectionExcel.error
 			Next
 			db.SaveChanges()
+			Me.ImportButton.Enabled = Not _error
 		Catch ex As Exception
 
 		End Try
 	End Sub
 
+	Private Sub ImportButton_Click(sender As Object, e As EventArgs) Handles ImportButton.Click
+		Dim db As New bridgemanagementEntities
+		Me.addInspection(db)
+		db.inspectionexcel.RemoveRange(db.inspectionexcel)
+		db.SaveChanges()
+		Me.Close()
+	End Sub
+
+	Private Sub addInspection(db As bridgemanagementEntities)
+		Dim inspectionExcels = db.inspectionexcel
+
+		For Each inspectionExcel In inspectionExcels
+			Dim newEntity As New inspection
+			Dim findEntity = db.inspection.Find(inspectionExcel.id)
+			If findEntity Is Nothing Then
+				newEntity.id = inspectionExcel.id
+				newEntity.recorddate = Now
+				newEntity.recordprogram = Me.Name
+				newEntity.recorduserid = "system"
+				newEntity.recordvalid = True
+				newEntity.inspectionyear = inspectionExcel.inspectionyear
+				newEntity.inspectioner = inspectionExcel.inspectioner
+				newEntity.undercondition = inspectionExcel.undercondition
+				newEntity.alternatepath = inspectionExcel.alternatepath
+				newEntity.generalroad = inspectionExcel.generalroad
+				newEntity.emergencyroad = inspectionExcel.emergencyroad
+				newEntity.occupancy = inspectionExcel.occupancy
+				newEntity.soundness = inspectionExcel.soundness
+				newEntity.uppermaterial = inspectionExcel.uppermaterial
+				newEntity.undermaterial = inspectionExcel.undermaterial
+				newEntity.bearing = inspectionExcel.bearing
+				newEntity.face = inspectionExcel.face
+				newEntity.datasavedir = Me.moveDirectory(inspectionExcel, newEntity)
+				db.inspection.Add(newEntity)
+				Me.addInspectionHistory(db, newEntity)
+			Else
+				findEntity.recorddate = Now
+				findEntity.recordprogram = Me.Name
+				findEntity.recorduserid = "system"
+				findEntity.recordvalid = True
+				findEntity.inspectionyear = inspectionExcel.inspectionyear
+				findEntity.inspectioner = inspectionExcel.inspectioner
+				findEntity.undercondition = inspectionExcel.undercondition
+				findEntity.alternatepath = inspectionExcel.alternatepath
+				findEntity.generalroad = inspectionExcel.generalroad
+				findEntity.emergencyroad = inspectionExcel.emergencyroad
+				findEntity.occupancy = inspectionExcel.occupancy
+				findEntity.soundness = inspectionExcel.soundness
+				findEntity.uppermaterial = inspectionExcel.uppermaterial
+				findEntity.undermaterial = inspectionExcel.undermaterial
+				findEntity.bearing = inspectionExcel.bearing
+				findEntity.face = inspectionExcel.face
+				findEntity.datasavedir = Me.moveDirectory(inspectionExcel, findEntity)
+				Me.addInspectionHistory(db, findEntity)
+			End If
+		Next
+	End Sub
+
+	Private Sub addInspectionHistory(db As bridgemanagementEntities, inspection As inspection)
+		Dim inspectionHistory As New inspectionhistory
+		inspectionHistory.id = inspection.id
+		inspectionHistory.recorddate = inspection.recorddate
+		inspectionHistory.recordprogram = inspection.recordprogram
+		inspectionHistory.recorduserid = inspection.recorduserid
+		inspectionHistory.recordvalid = inspection.recordvalid
+		inspectionHistory.inspectionyear = inspection.inspectionyear
+		inspectionHistory.inspectioner = inspection.inspectioner
+		inspectionHistory.undercondition = inspection.undercondition
+		inspectionHistory.alternatepath = inspection.alternatepath
+		inspectionHistory.generalroad = inspection.generalroad
+		inspectionHistory.emergencyroad = inspection.emergencyroad
+		inspectionHistory.occupancy = inspection.occupancy
+		inspectionHistory.soundness = inspection.soundness
+		inspectionHistory.uppermaterial = inspection.uppermaterial
+		inspectionHistory.undermaterial = inspection.undermaterial
+		inspectionHistory.bearing = inspection.bearing
+		inspectionHistory.face = inspection.face
+		inspectionHistory.datasavedir = inspection.datasavedir
+		db.inspectionhistory.Add(inspectionHistory)
+	End Sub
+
+	Private Function moveDirectory(inspectionExcel As inspectionexcel, inspection As inspection) As String
+		Dim setting As Setting = Setting.getInstance
+		Dim saveDirectory = Path.Combine(setting.SaveDirectoryPath, Format(inspection.recorddate, "yyyyMMddHHmmss"))
+		Directory.CreateDirectory(saveDirectory)
+		Dim dir As New DirectoryInfo(inspectionExcel.datadir)
+		Dim moveDirectoryTo = Path.Combine(saveDirectory, dir.Name)
+		dir.MoveTo(moveDirectoryTo)
+		Return moveDirectoryTo
+	End Function
 End Class
