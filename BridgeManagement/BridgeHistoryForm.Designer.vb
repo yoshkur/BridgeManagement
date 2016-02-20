77e34dcf7ed1b100ed4c60a78e2048b373247b42
yoshkur <yosh@1997.jukuin.keio.ac.jp>
2016-02-21 00:49:06 +0900

    項目見出しを変更。

--- BridgeManagement/BridgeHistoryForm.Designer.vb
+++ BridgeManagement/BridgeHistoryForm.Designer.vb
@@ -24,11 +24,8 @@ Partial Class BridgeHistoryForm
 	Private Sub InitializeComponent()
 		Me.components = New System.ComponentModel.Container()
 		Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+		Me.BridgehistoryBindingSource = New System.Windows.Forms.BindingSource(Me.components)
 		Me.IdDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-		Me.RecorddateDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-		Me.RecordprogramDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-		Me.RecorduseridDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-		Me.RecordvalidDataGridViewCheckBoxColumn = New System.Windows.Forms.DataGridViewCheckBoxColumn()
 		Me.BridgenameDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
 		Me.BridgenamerubyDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
 		Me.RouteDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
@@ -44,7 +41,6 @@ Partial Class BridgeHistoryForm
 		Me.LowermaterialDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
 		Me.UnderconditionDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
 		Me.EmergencyroadDataGridViewTextBoxColumn = New System.Windows.Forms.DataGridViewTextBoxColumn()
-		Me.BridgehistoryBindingSource = New System.Windows.Forms.BindingSource(Me.components)
 		CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
 		CType(Me.BridgehistoryBindingSource, System.ComponentModel.ISupportInitialize).BeginInit()
 		Me.SuspendLayout()
@@ -55,7 +51,7 @@ Partial Class BridgeHistoryForm
 		Me.DataGridView1.AllowUserToDeleteRows = False
 		Me.DataGridView1.AutoGenerateColumns = False
 		Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-		Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn, Me.RecorddateDataGridViewTextBoxColumn, Me.RecordprogramDataGridViewTextBoxColumn, Me.RecorduseridDataGridViewTextBoxColumn, Me.RecordvalidDataGridViewCheckBoxColumn, Me.BridgenameDataGridViewTextBoxColumn, Me.BridgenamerubyDataGridViewTextBoxColumn, Me.RouteDataGridViewTextBoxColumn, Me.AddressDataGridViewTextBoxColumn, Me.LatitudeDataGridViewTextBoxColumn, Me.LongitudeDataGridViewTextBoxColumn, Me.MadeDataGridViewTextBoxColumn, Me.BridgelengthDataGridViewTextBoxColumn, Me.BridgewidthDataGridViewTextBoxColumn, Me.SpanDataGridViewTextBoxColumn, Me.BridgestructureDataGridViewTextBoxColumn, Me.UppermaterialDataGridViewTextBoxColumn, Me.LowermaterialDataGridViewTextBoxColumn, Me.UnderconditionDataGridViewTextBoxColumn, Me.EmergencyroadDataGridViewTextBoxColumn})
+		Me.DataGridView1.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.IdDataGridViewTextBoxColumn, Me.BridgenameDataGridViewTextBoxColumn, Me.BridgenamerubyDataGridViewTextBoxColumn, Me.RouteDataGridViewTextBoxColumn, Me.AddressDataGridViewTextBoxColumn, Me.LatitudeDataGridViewTextBoxColumn, Me.LongitudeDataGridViewTextBoxColumn, Me.MadeDataGridViewTextBoxColumn, Me.BridgelengthDataGridViewTextBoxColumn, Me.BridgewidthDataGridViewTextBoxColumn, Me.SpanDataGridViewTextBoxColumn, Me.BridgestructureDataGridViewTextBoxColumn, Me.UppermaterialDataGridViewTextBoxColumn, Me.LowermaterialDataGridViewTextBoxColumn, Me.UnderconditionDataGridViewTextBoxColumn, Me.EmergencyroadDataGridViewTextBoxColumn})
 		Me.DataGridView1.DataSource = Me.BridgehistoryBindingSource
 		Me.DataGridView1.Location = New System.Drawing.Point(13, 23)
 		Me.DataGridView1.Name = "DataGridView1"
@@ -64,149 +60,121 @@ Partial Class BridgeHistoryForm
 		Me.DataGridView1.Size = New System.Drawing.Size(1183, 428)
 		Me.DataGridView1.TabIndex = 0
 		'
+		'BridgehistoryBindingSource
+		'
+		Me.BridgehistoryBindingSource.DataSource = GetType(BridgeManagement.bridgehistory)
+		'
 		'IdDataGridViewTextBoxColumn
 		'
 		Me.IdDataGridViewTextBoxColumn.DataPropertyName = "id"
-		Me.IdDataGridViewTextBoxColumn.HeaderText = "id"
+		Me.IdDataGridViewTextBoxColumn.HeaderText = "国交省作業番号"
 		Me.IdDataGridViewTextBoxColumn.Name = "IdDataGridViewTextBoxColumn"
 		Me.IdDataGridViewTextBoxColumn.ReadOnly = True
-		'
-		'RecorddateDataGridViewTextBoxColumn
-		'
-		Me.RecorddateDataGridViewTextBoxColumn.DataPropertyName = "recorddate"
-		Me.RecorddateDataGridViewTextBoxColumn.HeaderText = "recorddate"
-		Me.RecorddateDataGridViewTextBoxColumn.Name = "RecorddateDataGridViewTextBoxColumn"
-		Me.RecorddateDataGridViewTextBoxColumn.ReadOnly = True
-		'
-		'RecordprogramDataGridViewTextBoxColumn
-		'
-		Me.RecordprogramDataGridViewTextBoxColumn.DataPropertyName = "recordprogram"
-		Me.RecordprogramDataGridViewTextBoxColumn.HeaderText = "recordprogram"
-		Me.RecordprogramDataGridViewTextBoxColumn.Name = "RecordprogramDataGridViewTextBoxColumn"
-		Me.RecordprogramDataGridViewTextBoxColumn.ReadOnly = True
-		'
-		'RecorduseridDataGridViewTextBoxColumn
-		'
-		Me.RecorduseridDataGridViewTextBoxColumn.DataPropertyName = "recorduserid"
-		Me.RecorduseridDataGridViewTextBoxColumn.HeaderText = "recorduserid"
-		Me.RecorduseridDataGridViewTextBoxColumn.Name = "RecorduseridDataGridViewTextBoxColumn"
-		Me.RecorduseridDataGridViewTextBoxColumn.ReadOnly = True
-		'
-		'RecordvalidDataGridViewCheckBoxColumn
-		'
-		Me.RecordvalidDataGridViewCheckBoxColumn.DataPropertyName = "recordvalid"
-		Me.RecordvalidDataGridViewCheckBoxColumn.HeaderText = "recordvalid"
-		Me.RecordvalidDataGridViewCheckBoxColumn.Name = "RecordvalidDataGridViewCheckBoxColumn"
-		Me.RecordvalidDataGridViewCheckBoxColumn.ReadOnly = True
 		'
 		'BridgenameDataGridViewTextBoxColumn
 		'
 		Me.BridgenameDataGridViewTextBoxColumn.DataPropertyName = "bridgename"
-		Me.BridgenameDataGridViewTextBoxColumn.HeaderText = "bridgename"
+		Me.BridgenameDataGridViewTextBoxColumn.HeaderText = "道路橋名"
 		Me.BridgenameDataGridViewTextBoxColumn.Name = "BridgenameDataGridViewTextBoxColumn"
 		Me.BridgenameDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'BridgenamerubyDataGridViewTextBoxColumn
 		'
 		Me.BridgenamerubyDataGridViewTextBoxColumn.DataPropertyName = "bridgenameruby"
-		Me.BridgenamerubyDataGridViewTextBoxColumn.HeaderText = "bridgenameruby"
+		Me.BridgenamerubyDataGridViewTextBoxColumn.HeaderText = "道路橋名(カナ)"
 		Me.BridgenamerubyDataGridViewTextBoxColumn.Name = "BridgenamerubyDataGridViewTextBoxColumn"
 		Me.BridgenamerubyDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'RouteDataGridViewTextBoxColumn
 		'
 		Me.RouteDataGridViewTextBoxColumn.DataPropertyName = "route"
-		Me.RouteDataGridViewTextBoxColumn.HeaderText = "route"
+		Me.RouteDataGridViewTextBoxColumn.HeaderText = "路線名"
 		Me.RouteDataGridViewTextBoxColumn.Name = "RouteDataGridViewTextBoxColumn"
 		Me.RouteDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'AddressDataGridViewTextBoxColumn
 		'
 		Me.AddressDataGridViewTextBoxColumn.DataPropertyName = "address"
-		Me.AddressDataGridViewTextBoxColumn.HeaderText = "address"
+		Me.AddressDataGridViewTextBoxColumn.HeaderText = "所在地"
 		Me.AddressDataGridViewTextBoxColumn.Name = "AddressDataGridViewTextBoxColumn"
 		Me.AddressDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'LatitudeDataGridViewTextBoxColumn
 		'
 		Me.LatitudeDataGridViewTextBoxColumn.DataPropertyName = "latitude"
-		Me.LatitudeDataGridViewTextBoxColumn.HeaderText = "latitude"
+		Me.LatitudeDataGridViewTextBoxColumn.HeaderText = "緯度"
 		Me.LatitudeDataGridViewTextBoxColumn.Name = "LatitudeDataGridViewTextBoxColumn"
 		Me.LatitudeDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'LongitudeDataGridViewTextBoxColumn
 		'
 		Me.LongitudeDataGridViewTextBoxColumn.DataPropertyName = "longitude"
-		Me.LongitudeDataGridViewTextBoxColumn.HeaderText = "longitude"
+		Me.LongitudeDataGridViewTextBoxColumn.HeaderText = "経度"
 		Me.LongitudeDataGridViewTextBoxColumn.Name = "LongitudeDataGridViewTextBoxColumn"
 		Me.LongitudeDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'MadeDataGridViewTextBoxColumn
 		'
 		Me.MadeDataGridViewTextBoxColumn.DataPropertyName = "made"
-		Me.MadeDataGridViewTextBoxColumn.HeaderText = "made"
+		Me.MadeDataGridViewTextBoxColumn.HeaderText = "架設年次"
 		Me.MadeDataGridViewTextBoxColumn.Name = "MadeDataGridViewTextBoxColumn"
 		Me.MadeDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'BridgelengthDataGridViewTextBoxColumn
 		'
 		Me.BridgelengthDataGridViewTextBoxColumn.DataPropertyName = "bridgelength"
-		Me.BridgelengthDataGridViewTextBoxColumn.HeaderText = "bridgelength"
+		Me.BridgelengthDataGridViewTextBoxColumn.HeaderText = "橋長"
 		Me.BridgelengthDataGridViewTextBoxColumn.Name = "BridgelengthDataGridViewTextBoxColumn"
 		Me.BridgelengthDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'BridgewidthDataGridViewTextBoxColumn
 		'
 		Me.BridgewidthDataGridViewTextBoxColumn.DataPropertyName = "bridgewidth"
-		Me.BridgewidthDataGridViewTextBoxColumn.HeaderText = "bridgewidth"
+		Me.BridgewidthDataGridViewTextBoxColumn.HeaderText = "幅員"
 		Me.BridgewidthDataGridViewTextBoxColumn.Name = "BridgewidthDataGridViewTextBoxColumn"
 		Me.BridgewidthDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'SpanDataGridViewTextBoxColumn
 		'
 		Me.SpanDataGridViewTextBoxColumn.DataPropertyName = "span"
-		Me.SpanDataGridViewTextBoxColumn.HeaderText = "span"
+		Me.SpanDataGridViewTextBoxColumn.HeaderText = "径間数"
 		Me.SpanDataGridViewTextBoxColumn.Name = "SpanDataGridViewTextBoxColumn"
 		Me.SpanDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'BridgestructureDataGridViewTextBoxColumn
 		'
 		Me.BridgestructureDataGridViewTextBoxColumn.DataPropertyName = "bridgestructure"
-		Me.BridgestructureDataGridViewTextBoxColumn.HeaderText = "bridgestructure"
+		Me.BridgestructureDataGridViewTextBoxColumn.HeaderText = "構造形式"
 		Me.BridgestructureDataGridViewTextBoxColumn.Name = "BridgestructureDataGridViewTextBoxColumn"
 		Me.BridgestructureDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'UppermaterialDataGridViewTextBoxColumn
 		'
 		Me.UppermaterialDataGridViewTextBoxColumn.DataPropertyName = "uppermaterial"
-		Me.UppermaterialDataGridViewTextBoxColumn.HeaderText = "uppermaterial"
+		Me.UppermaterialDataGridViewTextBoxColumn.HeaderText = "上部工使用材料"
 		Me.UppermaterialDataGridViewTextBoxColumn.Name = "UppermaterialDataGridViewTextBoxColumn"
 		Me.UppermaterialDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'LowermaterialDataGridViewTextBoxColumn
 		'
 		Me.LowermaterialDataGridViewTextBoxColumn.DataPropertyName = "lowermaterial"
-		Me.LowermaterialDataGridViewTextBoxColumn.HeaderText = "lowermaterial"
+		Me.LowermaterialDataGridViewTextBoxColumn.HeaderText = "床板材料"
 		Me.LowermaterialDataGridViewTextBoxColumn.Name = "LowermaterialDataGridViewTextBoxColumn"
 		Me.LowermaterialDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'UnderconditionDataGridViewTextBoxColumn
 		'
 		Me.UnderconditionDataGridViewTextBoxColumn.DataPropertyName = "undercondition"
-		Me.UnderconditionDataGridViewTextBoxColumn.HeaderText = "undercondition"
+		Me.UnderconditionDataGridViewTextBoxColumn.HeaderText = "路下条件"
 		Me.UnderconditionDataGridViewTextBoxColumn.Name = "UnderconditionDataGridViewTextBoxColumn"
 		Me.UnderconditionDataGridViewTextBoxColumn.ReadOnly = True
 		'
 		'EmergencyroadDataGridViewTextBoxColumn
 		'
 		Me.EmergencyroadDataGridViewTextBoxColumn.DataPropertyName = "emergencyroad"
-		Me.EmergencyroadDataGridViewTextBoxColumn.HeaderText = "emergencyroad"
+		Me.EmergencyroadDataGridViewTextBoxColumn.HeaderText = "緊急輸送道路"
 		Me.EmergencyroadDataGridViewTextBoxColumn.Name = "EmergencyroadDataGridViewTextBoxColumn"
 		Me.EmergencyroadDataGridViewTextBoxColumn.ReadOnly = True
-		'
-		'BridgehistoryBindingSource
-		'
-		Me.BridgehistoryBindingSource.DataSource = GetType(BridgeManagement.bridgehistory)
 		'
 		'BridgeHistoryForm
 		'
@@ -223,11 +191,8 @@ Partial Class BridgeHistoryForm
 	End Sub
 
 	Friend WithEvents DataGridView1 As DataGridView
+	Friend WithEvents BridgehistoryBindingSource As BindingSource
 	Friend WithEvents IdDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-	Friend WithEvents RecorddateDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-	Friend WithEvents RecordprogramDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-	Friend WithEvents RecorduseridDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-	Friend WithEvents RecordvalidDataGridViewCheckBoxColumn As DataGridViewCheckBoxColumn
 	Friend WithEvents BridgenameDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
 	Friend WithEvents BridgenamerubyDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
 	Friend WithEvents RouteDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
@@ -243,5 +208,4 @@ Partial Class BridgeHistoryForm
 	Friend WithEvents LowermaterialDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
 	Friend WithEvents UnderconditionDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
 	Friend WithEvents EmergencyroadDataGridViewTextBoxColumn As DataGridViewTextBoxColumn
-	Friend WithEvents BridgehistoryBindingSource As BindingSource
 End Class
