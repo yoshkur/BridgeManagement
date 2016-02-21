c14306f49b1804cc77e116b9e0aeec3e3441a6a7
yoshkur <yosh@1997.jukuin.keio.ac.jp>
2016-02-22 02:32:38 +0900

    橋梁管理台帳フォームの項目修正。

---
+++ BridgeManagement/BridgeAddForm.Designer.vb
@@ -0,0 +1,479 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class BridgeAddForm
+	Inherits System.Windows.Forms.Form
+
+    'フォームがコンポーネントの一覧をクリーンアップするために dispose をオーバーライドします。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+	Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+		Try
+			If disposing AndAlso components IsNot Nothing Then
+				components.Dispose()
+			End If
+		Finally
+			MyBase.Dispose(disposing)
+		End Try
+	End Sub
+
+    'Windows フォーム デザイナーで必要です。
+    Private components As System.ComponentModel.IContainer
+
+    'メモ: 以下のプロシージャは Windows フォーム デザイナーで必要です。
+    'Windows フォーム デザイナーを使用して変更できます。  
+    'コード エディターを使って変更しないでください。
+    <System.Diagnostics.DebuggerStepThrough()> _
+	Private Sub InitializeComponent()
+		Me.Label1 = New System.Windows.Forms.Label()
+		Me.TextBox1 = New System.Windows.Forms.TextBox()
+		Me.TextBox2 = New System.Windows.Forms.TextBox()
+		Me.Label2 = New System.Windows.Forms.Label()
+		Me.TextBox3 = New System.Windows.Forms.TextBox()
+		Me.Label3 = New System.Windows.Forms.Label()
+		Me.TextBox4 = New System.Windows.Forms.TextBox()
+		Me.Label4 = New System.Windows.Forms.Label()
+		Me.TextBox5 = New System.Windows.Forms.TextBox()
+		Me.Label5 = New System.Windows.Forms.Label()
+		Me.TextBox6 = New System.Windows.Forms.TextBox()
+		Me.Label6 = New System.Windows.Forms.Label()
+		Me.TextBox7 = New System.Windows.Forms.TextBox()
+		Me.Label7 = New System.Windows.Forms.Label()
+		Me.TextBox8 = New System.Windows.Forms.TextBox()
+		Me.Label8 = New System.Windows.Forms.Label()
+		Me.TextBox9 = New System.Windows.Forms.TextBox()
+		Me.Label9 = New System.Windows.Forms.Label()
+		Me.TextBox10 = New System.Windows.Forms.TextBox()
+		Me.Label10 = New System.Windows.Forms.Label()
+		Me.TextBox11 = New System.Windows.Forms.TextBox()
+		Me.Label11 = New System.Windows.Forms.Label()
+		Me.TextBox12 = New System.Windows.Forms.TextBox()
+		Me.Label12 = New System.Windows.Forms.Label()
+		Me.TextBox13 = New System.Windows.Forms.TextBox()
+		Me.Label13 = New System.Windows.Forms.Label()
+		Me.TextBox14 = New System.Windows.Forms.TextBox()
+		Me.Label14 = New System.Windows.Forms.Label()
+		Me.TextBox15 = New System.Windows.Forms.TextBox()
+		Me.Label15 = New System.Windows.Forms.Label()
+		Me.TextBox16 = New System.Windows.Forms.TextBox()
+		Me.Label16 = New System.Windows.Forms.Label()
+		Me.TextBox17 = New System.Windows.Forms.TextBox()
+		Me.Label17 = New System.Windows.Forms.Label()
+		Me.TextBox18 = New System.Windows.Forms.TextBox()
+		Me.Label18 = New System.Windows.Forms.Label()
+		Me.TextBox19 = New System.Windows.Forms.TextBox()
+		Me.Label19 = New System.Windows.Forms.Label()
+		Me.TextBox20 = New System.Windows.Forms.TextBox()
+		Me.Label20 = New System.Windows.Forms.Label()
+		Me.SuspendLayout()
+		'
+		'Label1
+		'
+		Me.Label1.AutoSize = True
+		Me.Label1.Location = New System.Drawing.Point(42, 22)
+		Me.Label1.Name = "Label1"
+		Me.Label1.Size = New System.Drawing.Size(38, 12)
+		Me.Label1.TabIndex = 0
+		Me.Label1.Text = "Label1"
+		'
+		'TextBox1
+		'
+		Me.TextBox1.Location = New System.Drawing.Point(172, 22)
+		Me.TextBox1.Name = "TextBox1"
+		Me.TextBox1.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox1.TabIndex = 1
+		'
+		'TextBox2
+		'
+		Me.TextBox2.Location = New System.Drawing.Point(172, 63)
+		Me.TextBox2.Name = "TextBox2"
+		Me.TextBox2.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox2.TabIndex = 3
+		'
+		'Label2
+		'
+		Me.Label2.AutoSize = True
+		Me.Label2.Location = New System.Drawing.Point(42, 63)
+		Me.Label2.Name = "Label2"
+		Me.Label2.Size = New System.Drawing.Size(38, 12)
+		Me.Label2.TabIndex = 2
+		Me.Label2.Text = "Label2"
+		'
+		'TextBox3
+		'
+		Me.TextBox3.Location = New System.Drawing.Point(172, 106)
+		Me.TextBox3.Name = "TextBox3"
+		Me.TextBox3.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox3.TabIndex = 5
+		'
+		'Label3
+		'
+		Me.Label3.AutoSize = True
+		Me.Label3.Location = New System.Drawing.Point(42, 106)
+		Me.Label3.Name = "Label3"
+		Me.Label3.Size = New System.Drawing.Size(38, 12)
+		Me.Label3.TabIndex = 4
+		Me.Label3.Text = "Label3"
+		'
+		'TextBox4
+		'
+		Me.TextBox4.Location = New System.Drawing.Point(172, 154)
+		Me.TextBox4.Name = "TextBox4"
+		Me.TextBox4.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox4.TabIndex = 7
+		'
+		'Label4
+		'
+		Me.Label4.AutoSize = True
+		Me.Label4.Location = New System.Drawing.Point(42, 154)
+		Me.Label4.Name = "Label4"
+		Me.Label4.Size = New System.Drawing.Size(38, 12)
+		Me.Label4.TabIndex = 6
+		Me.Label4.Text = "Label4"
+		'
+		'TextBox5
+		'
+		Me.TextBox5.Location = New System.Drawing.Point(172, 200)
+		Me.TextBox5.Name = "TextBox5"
+		Me.TextBox5.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox5.TabIndex = 9
+		'
+		'Label5
+		'
+		Me.Label5.AutoSize = True
+		Me.Label5.Location = New System.Drawing.Point(42, 200)
+		Me.Label5.Name = "Label5"
+		Me.Label5.Size = New System.Drawing.Size(38, 12)
+		Me.Label5.TabIndex = 8
+		Me.Label5.Text = "Label5"
+		'
+		'TextBox6
+		'
+		Me.TextBox6.Location = New System.Drawing.Point(172, 252)
+		Me.TextBox6.Name = "TextBox6"
+		Me.TextBox6.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox6.TabIndex = 11
+		'
+		'Label6
+		'
+		Me.Label6.AutoSize = True
+		Me.Label6.Location = New System.Drawing.Point(42, 252)
+		Me.Label6.Name = "Label6"
+		Me.Label6.Size = New System.Drawing.Size(38, 12)
+		Me.Label6.TabIndex = 10
+		Me.Label6.Text = "Label6"
+		'
+		'TextBox7
+		'
+		Me.TextBox7.Location = New System.Drawing.Point(172, 300)
+		Me.TextBox7.Name = "TextBox7"
+		Me.TextBox7.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox7.TabIndex = 13
+		'
+		'Label7
+		'
+		Me.Label7.AutoSize = True
+		Me.Label7.Location = New System.Drawing.Point(42, 300)
+		Me.Label7.Name = "Label7"
+		Me.Label7.Size = New System.Drawing.Size(38, 12)
+		Me.Label7.TabIndex = 12
+		Me.Label7.Text = "Label7"
+		'
+		'TextBox8
+		'
+		Me.TextBox8.Location = New System.Drawing.Point(172, 343)
+		Me.TextBox8.Name = "TextBox8"
+		Me.TextBox8.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox8.TabIndex = 15
+		'
+		'Label8
+		'
+		Me.Label8.AutoSize = True
+		Me.Label8.Location = New System.Drawing.Point(42, 343)
+		Me.Label8.Name = "Label8"
+		Me.Label8.Size = New System.Drawing.Size(38, 12)
+		Me.Label8.TabIndex = 14
+		Me.Label8.Text = "Label8"
+		'
+		'TextBox9
+		'
+		Me.TextBox9.Location = New System.Drawing.Point(172, 390)
+		Me.TextBox9.Name = "TextBox9"
+		Me.TextBox9.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox9.TabIndex = 17
+		'
+		'Label9
+		'
+		Me.Label9.AutoSize = True
+		Me.Label9.Location = New System.Drawing.Point(42, 390)
+		Me.Label9.Name = "Label9"
+		Me.Label9.Size = New System.Drawing.Size(38, 12)
+		Me.Label9.TabIndex = 16
+		Me.Label9.Text = "Label9"
+		'
+		'TextBox10
+		'
+		Me.TextBox10.Location = New System.Drawing.Point(172, 432)
+		Me.TextBox10.Name = "TextBox10"
+		Me.TextBox10.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox10.TabIndex = 19
+		'
+		'Label10
+		'
+		Me.Label10.AutoSize = True
+		Me.Label10.Location = New System.Drawing.Point(42, 432)
+		Me.Label10.Name = "Label10"
+		Me.Label10.Size = New System.Drawing.Size(44, 12)
+		Me.Label10.TabIndex = 18
+		Me.Label10.Text = "Label10"
+		'
+		'TextBox11
+		'
+		Me.TextBox11.Location = New System.Drawing.Point(172, 478)
+		Me.TextBox11.Name = "TextBox11"
+		Me.TextBox11.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox11.TabIndex = 21
+		'
+		'Label11
+		'
+		Me.Label11.AutoSize = True
+		Me.Label11.Location = New System.Drawing.Point(42, 478)
+		Me.Label11.Name = "Label11"
+		Me.Label11.Size = New System.Drawing.Size(44, 12)
+		Me.Label11.TabIndex = 20
+		Me.Label11.Text = "Label11"
+		'
+		'TextBox12
+		'
+		Me.TextBox12.Location = New System.Drawing.Point(172, 524)
+		Me.TextBox12.Name = "TextBox12"
+		Me.TextBox12.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox12.TabIndex = 23
+		'
+		'Label12
+		'
+		Me.Label12.AutoSize = True
+		Me.Label12.Location = New System.Drawing.Point(42, 524)
+		Me.Label12.Name = "Label12"
+		Me.Label12.Size = New System.Drawing.Size(44, 12)
+		Me.Label12.TabIndex = 22
+		Me.Label12.Text = "Label12"
+		'
+		'TextBox13
+		'
+		Me.TextBox13.Location = New System.Drawing.Point(172, 569)
+		Me.TextBox13.Name = "TextBox13"
+		Me.TextBox13.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox13.TabIndex = 25
+		'
+		'Label13
+		'
+		Me.Label13.AutoSize = True
+		Me.Label13.Location = New System.Drawing.Point(42, 569)
+		Me.Label13.Name = "Label13"
+		Me.Label13.Size = New System.Drawing.Size(44, 12)
+		Me.Label13.TabIndex = 24
+		Me.Label13.Text = "Label13"
+		'
+		'TextBox14
+		'
+		Me.TextBox14.Location = New System.Drawing.Point(172, 898)
+		Me.TextBox14.Name = "TextBox14"
+		Me.TextBox14.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox14.TabIndex = 39
+		'
+		'Label14
+		'
+		Me.Label14.AutoSize = True
+		Me.Label14.Location = New System.Drawing.Point(42, 898)
+		Me.Label14.Name = "Label14"
+		Me.Label14.Size = New System.Drawing.Size(44, 12)
+		Me.Label14.TabIndex = 38
+		Me.Label14.Text = "Label14"
+		'
+		'TextBox15
+		'
+		Me.TextBox15.Location = New System.Drawing.Point(172, 850)
+		Me.TextBox15.Name = "TextBox15"
+		Me.TextBox15.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox15.TabIndex = 37
+		'
+		'Label15
+		'
+		Me.Label15.AutoSize = True
+		Me.Label15.Location = New System.Drawing.Point(42, 850)
+		Me.Label15.Name = "Label15"
+		Me.Label15.Size = New System.Drawing.Size(44, 12)
+		Me.Label15.TabIndex = 36
+		Me.Label15.Text = "Label15"
+		'
+		'TextBox16
+		'
+		Me.TextBox16.Location = New System.Drawing.Point(172, 798)
+		Me.TextBox16.Name = "TextBox16"
+		Me.TextBox16.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox16.TabIndex = 35
+		'
+		'Label16
+		'
+		Me.Label16.AutoSize = True
+		Me.Label16.Location = New System.Drawing.Point(42, 798)
+		Me.Label16.Name = "Label16"
+		Me.Label16.Size = New System.Drawing.Size(44, 12)
+		Me.Label16.TabIndex = 34
+		Me.Label16.Text = "Label16"
+		'
+		'TextBox17
+		'
+		Me.TextBox17.Location = New System.Drawing.Point(172, 752)
+		Me.TextBox17.Name = "TextBox17"
+		Me.TextBox17.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox17.TabIndex = 33
+		'
+		'Label17
+		'
+		Me.Label17.AutoSize = True
+		Me.Label17.Location = New System.Drawing.Point(42, 752)
+		Me.Label17.Name = "Label17"
+		Me.Label17.Size = New System.Drawing.Size(44, 12)
+		Me.Label17.TabIndex = 32
+		Me.Label17.Text = "Label17"
+		'
+		'TextBox18
+		'
+		Me.TextBox18.Location = New System.Drawing.Point(172, 704)
+		Me.TextBox18.Name = "TextBox18"
+		Me.TextBox18.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox18.TabIndex = 31
+		'
+		'Label18
+		'
+		Me.Label18.AutoSize = True
+		Me.Label18.Location = New System.Drawing.Point(42, 704)
+		Me.Label18.Name = "Label18"
+		Me.Label18.Size = New System.Drawing.Size(44, 12)
+		Me.Label18.TabIndex = 30
+		Me.Label18.Text = "Label18"
+		'
+		'TextBox19
+		'
+		Me.TextBox19.Location = New System.Drawing.Point(172, 661)
+		Me.TextBox19.Name = "TextBox19"
+		Me.TextBox19.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox19.TabIndex = 29
+		'
+		'Label19
+		'
+		Me.Label19.AutoSize = True
+		Me.Label19.Location = New System.Drawing.Point(42, 661)
+		Me.Label19.Name = "Label19"
+		Me.Label19.Size = New System.Drawing.Size(44, 12)
+		Me.Label19.TabIndex = 28
+		Me.Label19.Text = "Label19"
+		'
+		'TextBox20
+		'
+		Me.TextBox20.Location = New System.Drawing.Point(172, 620)
+		Me.TextBox20.Name = "TextBox20"
+		Me.TextBox20.Size = New System.Drawing.Size(220, 19)
+		Me.TextBox20.TabIndex = 27
+		'
+		'Label20
+		'
+		Me.Label20.AutoSize = True
+		Me.Label20.Location = New System.Drawing.Point(42, 620)
+		Me.Label20.Name = "Label20"
+		Me.Label20.Size = New System.Drawing.Size(44, 12)
+		Me.Label20.TabIndex = 26
+		Me.Label20.Text = "Label20"
+		'
+		'BridgeAddForm
+		'
+		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+		Me.ClientSize = New System.Drawing.Size(457, 975)
+		Me.Controls.Add(Me.TextBox14)
+		Me.Controls.Add(Me.Label14)
+		Me.Controls.Add(Me.TextBox15)
+		Me.Controls.Add(Me.Label15)
+		Me.Controls.Add(Me.TextBox16)
+		Me.Controls.Add(Me.Label16)
+		Me.Controls.Add(Me.TextBox17)
+		Me.Controls.Add(Me.Label17)
+		Me.Controls.Add(Me.TextBox18)
+		Me.Controls.Add(Me.Label18)
+		Me.Controls.Add(Me.TextBox19)
+		Me.Controls.Add(Me.Label19)
+		Me.Controls.Add(Me.TextBox20)
+		Me.Controls.Add(Me.Label20)
+		Me.Controls.Add(Me.TextBox13)
+		Me.Controls.Add(Me.Label13)
+		Me.Controls.Add(Me.TextBox12)
+		Me.Controls.Add(Me.Label12)
+		Me.Controls.Add(Me.TextBox11)
+		Me.Controls.Add(Me.Label11)
+		Me.Controls.Add(Me.TextBox10)
+		Me.Controls.Add(Me.Label10)
+		Me.Controls.Add(Me.TextBox9)
+		Me.Controls.Add(Me.Label9)
+		Me.Controls.Add(Me.TextBox8)
+		Me.Controls.Add(Me.Label8)
+		Me.Controls.Add(Me.TextBox7)
+		Me.Controls.Add(Me.Label7)
+		Me.Controls.Add(Me.TextBox6)
+		Me.Controls.Add(Me.Label6)
+		Me.Controls.Add(Me.TextBox5)
+		Me.Controls.Add(Me.Label5)
+		Me.Controls.Add(Me.TextBox4)
+		Me.Controls.Add(Me.Label4)
+		Me.Controls.Add(Me.TextBox3)
+		Me.Controls.Add(Me.Label3)
+		Me.Controls.Add(Me.TextBox2)
+		Me.Controls.Add(Me.Label2)
+		Me.Controls.Add(Me.TextBox1)
+		Me.Controls.Add(Me.Label1)
+		Me.Name = "BridgeAddForm"
+		Me.Text = "BridgeAddForm"
+		Me.ResumeLayout(False)
+		Me.PerformLayout()
+
+	End Sub
+
+	Friend WithEvents Label1 As Label
+	Friend WithEvents TextBox1 As TextBox
+	Friend WithEvents TextBox2 As TextBox
+	Friend WithEvents Label2 As Label
+	Friend WithEvents TextBox3 As TextBox
+	Friend WithEvents Label3 As Label
+	Friend WithEvents TextBox4 As TextBox
+	Friend WithEvents Label4 As Label
+	Friend WithEvents TextBox5 As TextBox
+	Friend WithEvents Label5 As Label
+	Friend WithEvents TextBox6 As TextBox
+	Friend WithEvents Label6 As Label
+	Friend WithEvents TextBox7 As TextBox
+	Friend WithEvents Label7 As Label
+	Friend WithEvents TextBox8 As TextBox
+	Friend WithEvents Label8 As Label
+	Friend WithEvents TextBox9 As TextBox
+	Friend WithEvents Label9 As Label
+	Friend WithEvents TextBox10 As TextBox
+	Friend WithEvents Label10 As Label
+	Friend WithEvents TextBox11 As TextBox
+	Friend WithEvents Label11 As Label
+	Friend WithEvents TextBox12 As TextBox
+	Friend WithEvents Label12 As Label
+	Friend WithEvents TextBox13 As TextBox
+	Friend WithEvents Label13 As Label
+	Friend WithEvents TextBox14 As TextBox
+	Friend WithEvents Label14 As Label
+	Friend WithEvents TextBox15 As TextBox
+	Friend WithEvents Label15 As Label
+	Friend WithEvents TextBox16 As TextBox
+	Friend WithEvents Label16 As Label
+	Friend WithEvents TextBox17 As TextBox
+	Friend WithEvents Label17 As Label
+	Friend WithEvents TextBox18 As TextBox
+	Friend WithEvents Label18 As Label
+	Friend WithEvents TextBox19 As TextBox
+	Friend WithEvents Label19 As Label
+	Friend WithEvents TextBox20 As TextBox
+	Friend WithEvents Label20 As Label
+End Class
---
+++ BridgeManagement/BridgeAddForm.vb
@@ -0,0 +1,3 @@
+﻿Public Class BridgeAddForm
+
+End Class
